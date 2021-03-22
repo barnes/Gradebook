53e8a50443ffc73948c8a2497083aa490d55dec7
Ryan Barnes <rybarnes@gmail.com>
2021-03-21 21:46:04 -0400

    v-for working

--- quasardo/src/pages/Test.vue
+++ quasardo/src/pages/Test.vue
@@ -5,6 +5,16 @@
       <q-input filled v-model="newTask" label="Filled" counter />
       <q-btn @click="onClick" color="secondary" label="Secondary" :disable="!newTask" />
     </div>
+
+    <q-list bordered separator>
+      <q-item v-for="task in tasks" :key="task.name">
+        <q-item-section>{{ task.name }}</q-item-section>
+      </q-item>
+
+      <q-item>
+        <q-item-section>Single line item</q-item-section>
+      </q-item>
+    </q-list>
     
     
   </q-page>
@@ -15,7 +25,11 @@ export default {
   name: 'Test',
   data () {
     return {
-      newTask: ''
+      newTask: '',
+      tasks: [
+        {name: 'task 1'},
+        {name: 'task 2'}
+      ]
     }
   },
   methods: {
